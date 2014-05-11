11070188499810c6e0af3a9d53beb7509400b5b2
Hiromi Ishii <konn.jinro@gmail.com>
2014-05-11 17:56:32 +0900

    improved documents

--- src/Numeric/Algebra/Domain.hs
+++ src/Numeric/Algebra/Domain.hs
@@ -3,6 +3,7 @@ module Numeric.Algebra.Domain where
 import Numeric.Ring.Class
 
 -- | Integral domain. This should satisfy following property:
+--
 --   prop> n * m == 0 ==> n == 0 || m == 0
 class Ring r => Domain r
 
