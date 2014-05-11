d15fc9d376fb185b9e61341e27e90c0fbb7dce81
Hiromi Ishii <konn.jinro@gmail.com>
2014-05-11 17:56:23 +0900

    implemented extended euclidean algorithm for general Euclidean domain

--- src/Numeric/Algebra/Domain/Euclidean.hs
+++ src/Numeric/Algebra/Domain/Euclidean.hs
@@ -8,34 +8,40 @@ import           Prelude                (Eq (..), Maybe (..), abs, fst, head,
 import           Prelude                (Integer, fromIntegral, ($))
 import qualified Prelude                as P
 
+infixl 7 `quot`, `rem`
+infix  7 `divide`
 class (DecidableZero r, Domain r) => Euclidean r where
   -- | Euclidean (degree) function on @r@.
   degree :: r -> Maybe Natural
-  -- | Division algorithm.
-
+  -- | Division algorithm. @a `divide` b@ calculates
+  --   quotient and reminder of @a@ divided by @b@.
+  --
+  -- prop> divide a p == (q, r) ==> p*q + r == a && degree r < degree q
   divide :: r                   -- ^ elements divided by
          -> r                   -- ^ divisor
-         -> (r                  -- ^ quotient
-            ,r                  -- ^ reminder
-            )
-  div :: r -> r -> r
-  div a b = fst $ a `divide` b
-  mod :: r -> r -> r
-  mod a b = snd $ a `divide` b
+         -> (r,r)               -- ^ quotient and remin
+  quot :: r -> r -> r
+  quot a b = fst $ a `divide` b
+  rem :: r -> r -> r
+  rem a b = snd $ a `divide` b
+  -- | @'gcd' a b@ calculates greatest common divisor of @a@ and @b@.
   gcd :: r -> r -> r
   gcd a b = head (euclid a b)^._1
   -- | Extended euclidean algorithm.
+  --
+  -- prop> euclid f g == xs ==> all (\(r, s, t) -> r == f * s + g * t) xs
   euclid :: r -> r -> [(r,r,r)]
   euclid f g = step [(g, zero, one), (f, one, zero)]
     where
       step acc @ ((r',s',t'):(r,s,t):_)
         | isZero r' = P.tail acc
         | otherwise =
-          let q   = r `div` r'
+          let q   = r `quot` r'
               r'' = r - q * r'
               s'' = s - q * s'
               t'' = t - q * t'
           in step ((r'', s'', t'') : acc)
+  {-# MINIMAL degree, divide #-}
 
 instance Euclidean Integer where
   degree = Just . fromIntegral . abs
